14dc9b8fb4452a7cacbb91a8bc2a4a829c59e5df
yappa <julijaogr@ukr.net>
2024-01-11 10:47:30 +0200

    [update] made help text bold

--- src/components/helper/helper_ui.rs
+++ src/components/helper/helper_ui.rs
@@ -1,18 +1,24 @@
 use tui::{
     backend::Backend,
+    layout::{Alignment, Rect},
+    style::{Modifier, Style},
+    widgets::{Block, BorderType, Borders, Paragraph},
     Frame,
-    layout::{ Rect, Alignment },
-    widgets::{ Block, Borders, BorderType, Paragraph },
 };
 
-use crate::application::{ app::App, mode::AppMode };
+use crate::application::{app::App, mode::AppMode};
 
 pub fn ui<B: Backend>(app: &mut App, f: &mut Frame<B>, chunks: &Vec<Rect>) {
     let helper_text = get_helper_text(&app);
 
-    let block = Block::default().borders(Borders::ALL).border_type(BorderType::Rounded);
+    let block = Block::default()
+        .borders(Borders::ALL)
+        .border_type(BorderType::Rounded);
 
-    let paragraph = Paragraph::new(helper_text).block(block).alignment(Alignment::Center);
+    let paragraph = Paragraph::new(helper_text)
+        .style(Style::default().add_modifier(Modifier::BOLD))
+        .block(block)
+        .alignment(Alignment::Center);
 
     f.render_widget(paragraph, chunks[2]);
 }
@@ -29,14 +35,15 @@ fn get_helper_text(app: &App) -> &'static str {
         AppMode::DirPriority => app.components.dir_priority.get_helper_text(),
         AppMode::DirFilePriority => app.components.dir_file_priority.get_helper_text(),
         AppMode::FileFilterForm(mode) => app.components.file_filter_form.get_helper_text(&mode),
-        AppMode::DirFilePriorityForm(mode) =>
-            app.components.dir_file_priority_form.get_helper_text(&mode),
+        AppMode::DirFilePriorityForm(mode) => {
+            app.components.dir_file_priority_form.get_helper_text(&mode)
+        }
         AppMode::DirPriorityForm(mode) => app.components.dir_priority_form.get_help_text(&mode),
         AppMode::FilePriorityForm(mode) => app.components.file_priority_form.get_help_text(&mode),
         AppMode::TemplateForm(mode) => app.components.template_form.get_helper_text(&mode),
         AppMode::TemplateList => app.components.template_list.get_helper_text(),
         AppMode::SchedulerForm(mode) => app.components.scheduler_form.get_helper_text(&mode),
         AppMode::SchedulerList => app.components.scheduler_list.get_helper_text(),
-        AppMode::FileListSettings => "", 
+        AppMode::FileListSettings => "",
     }
 }
